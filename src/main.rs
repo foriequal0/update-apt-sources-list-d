321b82e0208314abd5cfcc59612bd057c184d213
SeongChan Lee <foriequal@gmail.com>
2025-04-12 18:45:32 +0900

    Update DEB822 format, plucky 25.04

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,30 @@ use std::path::Path;
 use std::str::FromStr;
 
 use anyhow::{Context, Result};
+use deb822_lossless::{Deb822, Paragraph};
+
+#[derive(Eq, PartialEq, Hash)]
+struct Release<'a> {
+    name: &'a str,
+    version: &'a str,
+}
 
 #[rustfmt::skip]
-const RELEASES: &[&str] = &[
-    // 12.04
-    "precise", "quantal", "raring", "saucy", 
-    // 14.04
-    "trusty", "utopic", "vivid", "wily", 
-    // 16.04
-    "xenial", "yakkety", "zesty", "artful", 
-    // 18.04
-    "bionic", "cosmic", "disco", "eoan",
-    // 20.04
-    "focal", "groovy", "hirsute", "impish",
-    // 22.04
-    "jammy", "kinetic", "lunar", "mantic",
+const RELEASES: &[Release<'static>] = &[
+    Release { name: "precise", version: "12.04" }, Release { name: "quantal", version: "12.10" },
+    Release { name: "raring", version: "13.04" }, Release { name: "saucy", version: "13.10" },
+    Release { name: "trusty", version: "14.04" }, Release { name: "utopic", version: "14.10" },
+    Release { name: "vivid", version: "15.04" }, Release { name: "wily", version: "15.10" },
+    Release { name: "xenial", version: "16.04" }, Release { name: "yakkety", version: "16.10" },
+    Release { name: "zesty", version: "17.04" }, Release { name: "artful", version: "17.10" },
+    Release { name: "bionic", version: "18.04" }, Release { name: "cosmic", version: "18.10" },
+    Release { name: "disco", version: "19.04" }, Release { name: "eoan", version: "19.10" },
+    Release { name: "focal", version: "20.04" }, Release { name: "groovy", version: "20.10" },
+    Release { name: "hirsute", version: "21.04" }, Release { name: "impish", version: "21.10" },
+    Release { name: "jammy", version: "22.04" }, Release { name: "kinetic", version: "22.10" },
+    Release { name: "lunar", version: "23.04" }, Release { name: "mantic", version: "23.10" },
+    Release { name: "noble", version: "24.04" }, Release { name: "oracular", version: "24.10" },
+    Release { name: "plucky", version: "25.04" },
     // TODO: Add future releases
 ];
 
@@ -33,7 +42,7 @@ fn main() -> Result<()> {
     for entry in read_dir(path)? {
         let path_buf = entry?.path();
         let path = path_buf.to_str().context("path is not utf-8")?;
-        if !path.ends_with(".list") {
+        if !path.ends_with(".sources") {
             trace!("Skip {}", path);
             continue;
         }
@@ -52,180 +61,110 @@ fn update_file(path: &Path) -> Result<()> {
     info!("Read {}", path.to_str().unwrap());
 
     let content = std::fs::read_to_string(path)?;
-    let mut outputs = Vec::new();
-    let mut updated_sources = Vec::new();
-    for original_line in content.lines() {
-        let (line, uncommented) = if let Some(line) = disabled_on_upgrade(original_line) {
-            (line, true)
-        } else {
-            (original_line, false)
-        };
-
-        let source = match Source::from_str(line) {
-            Ok(source) => source,
-            Err(_) => {
-                // preserve the original line if failed to parse
-                outputs.push(original_line.to_string());
-                continue;
-            }
-        };
-
-        let (source, updated) =
-            if let Some((new_source, old_source)) = try_update_source(source.clone()) {
-                // Use updated source
-                info!("old: {}", old_source.to_string());
-                info!("updated: {} -> {}", old_source.dist, new_source.dist);
-                debug!("new: {}", new_source.to_string());
-                debug!("new release url: {}", new_source.release_url());
-                (Some(new_source), true)
-            } else if source.is_ok() {
-                // Current source is just working. Use it.
-                (Some(source), false)
-            } else {
-                warn!("Doesn't work: {}", source.to_string());
-                (None, false)
-            };
-
-        if let Some(source) = source {
-            outputs.push(source.to_string());
-            if uncommented || updated {
-                updated_sources.push((source, uncommented, updated));
-            }
-        } else {
-            outputs.push(original_line.to_string());
-        }
-    }
-    let mut final_output: String = outputs.join("\n");
-    if !final_output.ends_with("\n") {
-        final_output.push_str("\n");
+    let deb822 = Deb822::from_str(&content)?;
+    let mut updated = false;
+    for mut paragraph in deb822.paragraphs() {
+        updated |= try_update_paragraph(&mut paragraph)?;
     }
 
-    if !updated_sources.is_empty() {
-        println!("Updated {}", path.to_string_lossy());
-        for (source, uncommented, updated) in updated_sources {
-            println!(
-                "  {}, uncommented: {}, updated: {}",
-                source.to_string(),
-                uncommented,
-                updated
-            );
-        }
-        let mut bak = path.to_path_buf();
-        bak.set_extension("list.bak");
-        std::fs::copy(path, bak).context("Failed to copy backup")?;
-        std::fs::write(path, final_output.as_bytes()).context("Failed to write new output")?;
+    if !updated {
+        return Ok(());
     }
+
+    println!("Updated {}", path.to_string_lossy());
+
+    let mut bak = path.to_path_buf();
+    bak.set_extension("soruces.bak");
+    std::fs::copy(path, bak).context("Failed to copy backup")?;
+    std::fs::write(path, deb822.to_string().as_bytes()).context("Failed to write new output")?;
+
     Ok(())
 }
 
-fn disabled_on_upgrade(line: &str) -> Option<&str> {
-    let line = line.trim();
-    if line.starts_with("#") && line.contains("disabled on upgrade") {
-        Some(line.trim_start_matches("#"))
-    } else {
-        None
+fn try_update_paragraph(paragraph: &mut Paragraph) -> Result<bool> {
+    if get_http_uri(paragraph).is_none() {
+        return Ok(false);
+    };
+
+    let mut updated = false;
+    if paragraph.contains_key("Enabled") {
+        updated = true;
+        paragraph.remove("Enabled");
     }
+
+    if try_update_by_suites_name(paragraph)? {
+        return Ok(true);
+    }
+
+    Ok(updated)
 }
 
-fn try_update_source(mut source: Source) -> Option<(Source, Source)> {
-    let old_source = source.clone();
-    let mut old_dist = None;
-    let mut new_source = None;
-    for i in 0..RELEASES.len() - 1 {
-        let current = RELEASES[i];
-        let next = RELEASES[i + 1];
+fn try_update_by_suites_name(paragraph: &mut Paragraph) -> Result<bool> {
+    let Some(uris) = get_http_uri(paragraph) else {
+        return Ok(false);
+    };
 
-        if !source.dist.contains(current) {
-            continue;
-        }
-        if old_dist.is_none() {
-            old_dist = Some(source.dist.clone());
-        }
+    let Some(mut suites) = paragraph.get("Suites") else {
+        return Ok(false);
+    };
 
-        source.dist = source.dist.replace(current, next);
-
-        if source.is_ok() {
-            new_source = Some(source.clone());
-        }
+    let range = get_release_range_by_name(&suites);
+    if range.is_empty() {
+        return Ok(false);
     }
 
-    if let Some(new_source) = new_source {
-        Some((new_source, old_source))
-    } else {
-        None
+    let mut updated = false;
+    for i in 1..range.len() {
+        let prev = &range[i - 1];
+        let current = &range[i];
+        suites = suites.replace(prev.name, current.name);
+        let mut all = true;
+        for suite in suites.split_ascii_whitespace() {
+            if !is_available(&uris, suite) {
+                all = false;
+                break;
+            }
+        }
+
+        if !all {
+            break;
+        }
+
+        updated = true;
+        paragraph.set("Suites", &suites);
     }
+
+    Ok(updated)
 }
 
-#[derive(Clone, Debug)]
-struct Source {
-    archive_type: String,
-    arch: Option<String>,
-    url: String,
-    dist: String,
-    components: Vec<String>,
+fn get_release_range_by_name(target: &str) -> &'static [Release<'static>] {
+    for i in 0..RELEASES.len() {
+        let release = &RELEASES[i];
+        if target.contains(release.name) {
+            return &RELEASES[i..];
+        }
+    }
+
+    &[]
 }
 
-impl Source {
-    fn to_string(&self) -> String {
-        let mut output: Vec<&str> = Vec::new();
-        output.push(&self.archive_type);
-        if let Some(arch) = self.arch.as_ref() {
-            output.push(&arch);
-        }
-        output.push(&self.url);
-        output.push(&self.dist);
-        for items in self.components.iter() {
-            output.push(items);
-        }
-        output.join(" ")
+fn get_http_uri(paragraph: &Paragraph) -> Option<String> {
+    let uri = paragraph.get("URIs")?;
+
+    if uri.starts_with("http://") {
+        return Some(uri);
+    }
+    if uri.starts_with("https://") {
+        return Some(uri);
     }
 
-    fn release_url(&self) -> String {
-        format!("{}/dists/{}/Release", self.url, self.dist)
-    }
-
-    fn is_ok(&self) -> bool {
-        reqwest::blocking::get(&self.release_url())
-            .and_then(|x| x.error_for_status())
-            .is_ok()
-    }
+    None
 }
 
-impl FromStr for Source {
-    type Err = ();
+fn is_available(uri: &str, suite: &str) -> bool {
+    let release_url = format!("{uri}/dists/{suite}/Release");
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let s = s.trim();
-        let s = &s[0..s.find('#').unwrap_or(s.len())];
-        if s.is_empty() {
-            return Err(());
-        }
-        let items: Vec<_> = s.split_whitespace().collect();
-        if items.len() < 2 // To short to even determine
-            || (items[1].starts_with("[") && items.len() < 4) // have arch but too short
-            || items.len() < 3
-        // doesn't have arch but too short
-        {
-            return Err(());
-        }
-        if !matches!(items.get(0), Some(&"deb") | Some(&"deb-src")) {
-            return Err(());
-        }
-        let archive_type = items[0].to_string();
-        let (arch, rest) = if items[1].starts_with("[") {
-            (Some(items[1].to_string()), &items[2..])
-        } else {
-            (None, &items[1..])
-        };
-        let url = rest[0].to_string();
-        let dist = rest[1].to_string();
-        let components = rest[2..].iter().map(|x| x.to_string()).collect();
-        Ok(Source {
-            archive_type,
-            arch,
-            url,
-            dist,
-            components,
-        })
-    }
+    reqwest::blocking::get(&release_url)
+        .and_then(|x| x.error_for_status())
+        .is_ok()
 }
